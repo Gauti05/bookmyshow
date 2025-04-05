c160626c38052c1292689b489c1c2e82d38fb8dd
Gautam <gautamrawat52002gmail.com>
2025-04-05 12:52:15 +0530

    change

--- server/server.js
+++ server/server.js
@@ -78,5 +78,5 @@ bookingRoutes(app)
 
 
 app.listen(process.env.PORT, '0.0.0.0', () => {
-    console.log(`Server running on http://0.0.0.0:${PORT}`);
+    console.log(`Server running on http://0.0.0.0:${process.env.PORT}`);
 });
