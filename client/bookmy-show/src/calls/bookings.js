c1452999dc8caac9da88cfb687ebed115be008e0
Gautam <gautamrawat52002gmail.com>
2025-04-05 13:08:49 +0530

    updated

--- client/bookmy-show/src/calls/bookings.js
+++ client/bookmy-show/src/calls/bookings.js
@@ -2,7 +2,7 @@ import axiosInstance from ".";
 
 export async function MakePayment(data){
     try{
-        const response =  await axiosInstance.post("https://bookmyshow-3-38cj.onrender.com/payment", {
+        const response =  await axiosInstance.post("http://localhost:8000/payment", {
             token:data.token.id,
             amount:data.amount
            
@@ -18,7 +18,7 @@ return err.response
 
 export async function CreateBooking(data){
     try{
-        const response =  await axiosInstance.post("https://bookmyshow-3-38cj.onrender.com/bookings", {
+        const response =  await axiosInstance.post("http://localhost:8000/bookings", {
             show:data.showId,
             seats:data.seats,
             transactionId : data.transactionId
--- client/bookmy-show/src/calls/shows.js
+++ client/bookmy-show/src/calls/shows.js
@@ -2,7 +2,7 @@ import axiosInstance from ".";
 
 export async function GetShowsForMovie(movieId, date){
     try{
-        const response =  await axiosInstance.get(`https://bookmyshow-3-38cj.onrender.com/movies/${movieId}/shows?date=${date}`)
+        const response =  await axiosInstance.get(`http://localhost:8000/movies/${movieId}/shows?date=${date}`)
         return response
         // console.log(response)
     }
@@ -15,7 +15,7 @@ return err.response
 
 export async function GetShowsDetailsById(showId){
     try{
-        const response =  await axiosInstance.get(`https://bookmyshow-3-38cj.onrender.com/shows/${showId}`)
+        const response =  await axiosInstance.get(`http://localhost:8000/shows/${showId}`)
         return response
         // console.log(response)
     }
