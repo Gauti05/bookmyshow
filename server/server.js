2ff50b1eeae47667bbc7e940b8ac8e16f917238a
Rishabh Rawat <rishabhrawat683@gmail.com>
2025-07-15 21:46:18 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -72,7 +72,7 @@ bookingRoutes(app)
 
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
